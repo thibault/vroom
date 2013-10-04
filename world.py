c72bf8468089637eac2f6231ff187718763b637d
Thibault Jouannic <thibault@miximum.fr>
2013-10-04 22:53:11 +0200

    Abstract motion and vector management

--- world.py
+++ world.py
@@ -5,6 +5,37 @@ import math
 white = (255, 255, 255)
 
 
+class Motion:
+    """Motion representation class."""
+
+    def __init__(self, angle, speed):
+        "angle in radius, speed in meter / second."""
+        self.angle = angle
+        self.speed = speed
+
+
+class Coordinates:
+    """Represents coordinates on the map."""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, motion, time):
+        """Add given motion to coordinates.
+
+        Time is given in milliseconds.
+
+        """
+        assert isinstance(motion, Motion)
+
+        angle = motion.angle
+        speed = motion.speed / 1000.0 * time
+        new_x = self.x + math.sin(angle) * speed
+        new_y = self.y + math.cos(angle) * speed
+
+        return Coordinates(new_x, new_y)
+
+
 class Universe:
     def __init__(self):
         self.cars = (
@@ -23,18 +54,14 @@ class Universe:
 
 class Car:
     def __init__(self, x, y, angle, speed):
-        self.coordinates = (x, y)
-        self.motion = (angle, speed)
+        self.coordinates = Coordinates(x, y)
+        self.motion = Motion(angle, speed)
+        self.width = 3
 
     def update(self, delta):
-        self.coordinates = self.integrate(self.coordinates, self.motion, delta)
-
-    def integrate(self, coordinates, motion, delta):
-        speed = motion[1] / 1000.0 * delta
-        new_x = coordinates[0] + math.sin(motion[0]) * speed
-        new_y = coordinates[1] + math.cos(motion[0]) * speed
-        return (new_x, new_y)
+        self.coordinates = self.coordinates.move(self.motion, delta)
 
     def draw(self, surface):
-        rect = pygame.Rect(int(self.coordinates[0]), int(self.coordinates[1]), 3, 3)
+        rect = pygame.Rect(self.coordinates.x, self.coordinates.y,
+                           self.width, self.width)
         pygame.draw.rect(surface, white, rect, 0)
