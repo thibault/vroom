09f2269c9baf3db1e5ad3c5ad8312120219c2f76
Thibault Jouannic <thibault@miximum.fr>
2013-10-04 19:17:13 +0200

    Basic car drawing

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import sys
 import pygame
 
+from world import Universe
+
+
+# Initialize pygame
 pygame.init()
-
 size = width, height = 800, 600
-speed = [2, 2]
 black = 0, 0, 0
-
 screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+# Initialize the universe
+universe = Universe()
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    # Game engine go here
-
     screen.fill(black)
+    universe.update(clock.get_time())
+    universe.draw(screen)
+
     pygame.display.flip()
+    clock.tick(30)
---
+++ world.py
@@ -0,0 +1,35 @@
+import pygame
+
+
+white = (255, 255, 255)
+
+
+class Universe:
+    def __init__(self):
+        self.cars = (
+            Car(100, 100, 90, 30),
+        )
+
+    def update(self, delta):
+        """Update the universe status."""
+        for car in self.cars:
+            car.update(delta)
+
+    def draw(self, surface):
+        for car in self.cars:
+            car.draw(surface)
+
+
+class Car:
+    def __init__(self, x, y, direction, speed):
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.speed = speed
+
+    def update(self, delta):
+        pass
+
+    def draw(self, surface):
+        rect = pygame.Rect(self.x, self.y, 10, 15)
+        pygame.draw.rect(surface, white, rect, 0)
